8300afb9fc97d049944ad4b20816fa67138576dd
Blessing <berachah54@gmail.com>
2024-04-09 04:30:54 +0100

    progress

---
+++ assets/js/main.js
@@ -0,0 +1,188 @@
+// Function to retrieve WordPress posts
+async function UnlimitedPostPluginretrieveWordPressPosts({ page = 1, perPage = 15, searchQuery = '', postType = 'post', orderBy = 'date', order = 'desc' }) {
+    if(page< 2){
+        console.log('[ UnlimitedPostPluginretrieveWordPressPosts ] Invalid error Posts')
+        return;
+    }
+    // Define the base URL of the WordPress site and the endpoint for posts
+    var baseURL = `${window.location.origin}/wp-json/wp/v2/posts`;
+
+    // varruct the query string with optional parameters
+
+    let queryParams = `?page=${page}&per_page=${perPage}&type=${postType}&orderby=${orderBy}&order=${order}&meta_key=_thumbnail_id`;
+    if (searchQuery !== '') {
+        queryParams += `&search=${encodeURIComponent(searchQuery)}`;
+    }
+
+    // varruct the complete URL
+    var url = `${baseURL}${queryParams}`;
+
+    try {
+        // Send a GET request to the WordPress REST API endpoint
+        var response = await fetch(url);
+
+        // Check if the request was successful (status code 200)
+        if (response.ok) {
+            // Parse the JSON response
+            var posts = await response.json();
+
+            // Return the retrieved posts
+            return posts;
+        } else {
+            // If the request was not successful, throw an error
+            throw new Error(`Failed to retrieve WordPress posts. Status: ${response.status}`);
+        }
+    } catch (error) {
+        // Handle any errors that occur during the request
+        console.error('Error retrieving WordPress posts:', error.message);
+        return null;
+    }
+}
+
+
+async function UnlimitedPostPluginInit() {
+
+    var buttonElement = document.querySelector('#unlimited_posts_display_posts_shortcode button');
+
+    if (!buttonElement) {
+        setTimeout(UnlimitedPostPluginInit, 1000)
+        return
+    }
+
+
+
+}
+
+async function UnlimitedPostPluginInitLoadMore() {
+    var buttonElement = document.querySelector('#unlimited_posts_display_posts_shortcode button');
+
+    var buttonString = buttonElement.dataset;
+    
+    if(buttonString.disabled){
+        console.log('End of list, button is disabled')
+        return
+    }
+    if(buttonString.loading){
+        console.log(' button is loading')
+        return
+    }
+    buttonElement.dataset.loading = true
+    buttonElement.textContent = 'fetching...'
+    // Example usage:
+    UnlimitedPostPluginretrieveWordPressPosts({ page:  buttonString.page, perPage: 12 })
+        .then(posts => {
+            if (posts) {
+                // Handle retrieved posts
+             
+                var template = document.createDocumentFragment();
+                if (Array.isArray(posts)) {
+             
+                    if(posts.length < buttonString.per_page) {
+                        UnlimitedPostPluginPostDeactivateButton();
+                    }
+                    posts.forEach(post => {
+                        var postcard = UnlimitedPostPluginPostCardSkeleton(post);
+
+                        template.appendChild(postcard)
+                    })
+                    var arrayCardElement = document.querySelector('#unlimited_posts_display_posts_shortcode .unlimited_posts-post-card');
+
+                    arrayCardElement.appendChild(template);
+                    buttonElement.dataset.page = buttonString.page + 1
+                }
+            } else {
+                // Handle case where posts couldn't be retrieved
+                console.log('Failed to retrieve WordPress posts.');
+            }
+        })
+        .catch(error => {
+            console.log({buttonString})
+            if(buttonString.page > 2 ){
+        UnlimitedPostPluginPostDeactivateButton();
+            }
+            console.error('Error:', error);
+        }).finally(() => {
+    buttonElement.dataset.loading = false
+    buttonElement.textContent = 'Load more'
+
+        });
+
+}
+
+function UnlimitedPostPluginPostDeactivateButton() {
+    console.log("UnlimitedPostPluginPostDeactivateButton")
+    var buttonElement = document.querySelector('#unlimited_posts_display_posts_shortcode button');
+
+    buttonElement.style.opacity = 0.5;
+    buttonElement.dataset.disabled = true;
+}
+function UnlimitedPostPluginPostCardSkeleton(post) {
+    // var wrapper = document.createElement('div');
+    // wrapper.classList.add('unlimited-post-listing-item');
+
+    // //create image link
+    // var image_link = document.createElement('a');
+    // image_link.classList.add('unlimited-post-listing-item-link');
+
+    // //create img
+    // var img = document.createElement('img');
+    // img.classList.add('unlimited-post-listing-item-image');
+    // img.width = '300';
+    // img.height = '150';
+    // img.alt = '';
+    // image_link.appendChild(img);
+
+    // //create link
+    // var title = document.createElement('a');
+    // title.classList.add('unlimited-post-listing-item-title');
+
+
+
+    // //create date
+    // var date = document.createElement('div');
+    // date.classList.add('unlimited-post-listing-item-date');
+    // var date_text = document.createElement('span');
+    // date.appendChild(date_text)
+
+    // //Wrappe append
+    // wrapper.appendChild(image_link)
+    // wrapper.appendChild(title)
+    // wrapper.appendChild(date)
+    if (!post.id) return;
+    if (!document.querySelector('.unlimited_posts-post-card .listing-item')) {
+        console.error('Node clone error');
+        return;
+    }
+    var wrapper = document.querySelector('.unlimited_posts-post-card .listing-item').cloneNode(true);
+    var imagelink = wrapper.querySelector('.unlimited-post-image_link');
+    imagelink.href = post.link;
+    imagelink.querySelector('img').src = post.attachment_url_image;
+
+    var title = wrapper.querySelector('.unlimited-post-listing-item-title');
+    title.href = post.link;
+    title.textContent = post.title.rendered;
+
+    const dateString = post.date;
+    const dateObj = new Date(dateString);
+
+    const options = { month: 'long', day: 'numeric', year: 'numeric' };
+    const formattedDate = dateObj.toLocaleDateString('en-US', options);
+    var date = wrapper.querySelector('.unlimited-post-listing-item-date');
+    date.textContent = formattedDate;
+
+    return wrapper;
+
+}
+console.log('[ v1.0.1 ]', 'UnlimitedPostPluginInitLoadMore');
+
+document.addEventListener('DOMContentLoaded', UnlimitedPostPluginInit)
+
+// <div class="listing-item">
+//     <a class="image" href="https://creatiosoftweb.pokermoogley.com/blogs/how-does-pokernow-club-work-and-how-can-creatiosoft-assist-you-in-creating-something-similar/">
+//         <img width="300" height="150" src="https://creatiosoftweb.pokermoogley.com/blogs/wp-content/uploads/2024/02/How-to-moneitize-your-mobile-gane-54.png" class="attachment-medium size-medium wp-post-image" alt="" decoding="async">
+//     </a> 
+//     <a class="title" style="min-height:180px" href="https://creatiosoftweb.pokermoogley.com/blogs/how-does-pokernow-club-work-and-how-can-creatiosoft-assist-you-in-creating-something-similar/">How does Pokernow Club work and how can Creatiosoft assist you in creating something similar?</a> 
+//     <div class="date" style="position:relative;">
+//     <span style="position:absolute;bottom:18px;">February 5, 2024</span>
+//     </div>
+// </div>
--- display-unlimited-posts.php
+++ display-unlimited-posts.php
@@ -34,28 +34,90 @@ add_shortcode('display-unlimited-posts', 'unlimited_posts_display_posts_shortcod
 function unlimited_posts_display_posts_shortcode($atts)
 {
 
-	// Pull in shortcode attributes and set defaults.
-	$atts = shortcode_atts(
-		array(
+    // Pull in shortcode attributes and set defaults.
+    $atts = shortcode_atts(
+        array(
             'order'                 => 'DESC',
-			'orderby'               => 'date',
-			'post_status'           => 'publish',
-			'post_type'             => 'post',
-			'posts_per_page'        => '12',
-		),
-		$atts,
-		'display-unlimited-posts'
-	);
+            'orderby'               => 'date',
+            'post_status'           => 'publish',
+            'post_type'             => 'post',
+            'posts_per_page'        => '12',
+            'meta_key'              =>  "_thumbnail_id"
+        ),
+        $atts,
+        'display-unlimited-posts'
+    );
 
-  $script=  '<div id="unlimited_posts_display_posts_shortcode">';
- 
-        $script .=  '<button data-page="1" data-per_page="'.$atts['posts_per_page'].'" data-post_type="'.$atts['post_type'].'"  data-post_status="'.$atts['post_status'].'" data-post_status="'.$atts['post_status'].'" data-order="'.$atts['order'].'" data-orderby="'.$atts['posts_orderby'].'"  >Load more</button>';
-        $script .=  '</div>';
+    $body = '<div id="unlimited_posts_display_posts_shortcode"><div class="unlimited_posts-post-card">';
+    global $dup_listing;
 
-return $script;
+    $dup_listing = new WP_Query($atts);
+    while ($dup_listing->have_posts()) :
+        $dup_listing->the_post();
+        global $post;
+        // start of card
+        $body .=  '<div class="listing-item">';
+        //image
+
+        $body .= '<a class="unlimited-post-image_link" href="' . get_permalink() . '">' . get_the_post_thumbnail(get_the_ID(), 'medium') . '</a> ';
+        //end of image
+
+        //start of title
+        $body .= '<a class="unlimited-post-listing-item-title" style="min-height:180px" href="' . apply_filters('the_permalink', get_permalink()) . '">' . get_the_title() . '</a>';
+        //end of title
+        // start of date
+        $body .= '<div class="unlimited-post-listing-item-date" style="position:relative;">
+
+            <span>' . get_the_date('F j, Y') . '</span>
+    </div>';
+        // end of date
+        // end of card
+        $body .= '</div>'; //end of card
+    endwhile;
+    wp_reset_postdata();
+    $body .=  '</div>'; //end of class unlimited_posts-post-card
+    $body .= '<div class="unlimited-posts-button-wrapper"><button data-page="2" data-per_page="' . $atts['posts_per_page'] . '" data-post_type="' . $atts['post_type'] . '"  data-post_status="' . $atts['post_status'] . '" data-post_status="' . $atts['post_status'] . '" data-order="' . $atts['order'] . '" data-orderby="' . $atts['orderby'] . '" data-metakey="' . $atts['meta_key'] . '" onclick="UnlimitedPostPluginInitLoadMore()">Load more</button></div>';
+    $body .=  '</div>';
+
+    return $body;
 }
-function  unlimited_posts_enqueue_custom_script() {
+function  unlimited_posts_enqueue_custom_script()
+{
     // Enqueue the custom script with a unique handle
-    wp_enqueue_script('unlimited_posts_enqueue_custom_script-custom-script', get_template_directory_uri() . '/custom-script.js', array(), '1.0', true);
+    wp_enqueue_script('unlimited_posts_enqueue_custom_script-custom-script', plugins_url('display-unlimited-posts/assets/js/main.js'), array(), '1.0.1', true);
 }
 add_action('wp_enqueue_scripts', 'unlimited_posts_enqueue_custom_script');
+// Enqueue stylesheet function
+function unlimited_posts_enqueue_custom_style()
+{
+    // Enqueue the custom stylesheet with a unique handle
+    wp_enqueue_style('unlimited_posts_enqueue_custom_script-custom-style', plugins_url('display-unlimited-posts/assets/css/main.css'), array(), '1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'unlimited_posts_enqueue_custom_style');
+
+
+
+add_action('rest_api_init', 'unlimited_posts_ws_register_images_field');
+
+function unlimited_posts_ws_register_images_field()
+{
+    register_rest_field(
+        'post',
+        'attachment_url_image',
+        array(
+            'get_callback'    => 'unlimited_posts_ws_get_images_urls',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function unlimited_posts_ws_get_images_urls($object, $field_name, $request)
+{
+
+    $medium = wp_get_attachment_image_src(get_post_thumbnail_id($object['id']), 'medium');
+    if (is_array($medium)) {
+        return isset($medium['0']) ? $medium['0'] : '';
+    }
+    return '';
+}
